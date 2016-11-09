8c45ab9d0c87085679084ffc33e2464eaa8ef56d
Maaz Ali <MaazAli@users.noreply.github.com>
2016-11-08 22:25:23 -0500

    Updated user branch from Portal workspace (server script)

--- serverscript.js
+++ serverscript.js
@@ -16,4 +16,19 @@ function createEvent() {
     var startTimestamp = args.Get("startTimestamp");
     var endTimestamp = args.Get("endTimestamp");
     var capacity = args.Get("capacity");
+    console.log(groupId);
+    
+    db.Execute('INSERT INTO events VALUES(@groupId, @title, @description, @startTimestamp, @endTimestamp, @capacity, @currentUser');
+    return getData();
+}
+
+// Create group
+function createGroup() {
+	var groupId = args.Get("groupId");
+    var title = args.Get("title");
+    var description = args.Get("description");
+    var startTimestamp = args.Get("startTimestamp");
+    var endTimestamp = args.Get("endTimestamp");
+    var capacity = args.Get("capacity");
+    console.log(groupId);
 }
